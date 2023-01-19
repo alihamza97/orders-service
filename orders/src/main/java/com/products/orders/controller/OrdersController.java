20100ab5cbb24c3aedf626a7990a46fab34199ce
Ali Hamza <tkfreak01@gmail.com>
2023-01-19 12:25:01 +0100

    Update OrdersController.java

--- orders/src/main/java/com/products/orders/controller/OrdersController.java
+++ orders/src/main/java/com/products/orders/controller/OrdersController.java
@@ -29,12 +29,12 @@ public class OrdersController {
 	
 	 @PostMapping("/create-order")
 	    public Map<String, Integer> getGreeting(@RequestBody Order order) {
-	        final Map<String, Integer> result = new HashMap<>();
+	        final Map<String, Integer> orderMap = new HashMap<>();
 	        ordersService.createProduct(order);
 	        int orderNumber=order.getOrderNumber();
-	        result.put("orderNumber", orderNumber);
+	        orderMap.put("orderNumber", orderNumber);
 
-	        return result;
+	        return orderMap;
 	    }
 
 
